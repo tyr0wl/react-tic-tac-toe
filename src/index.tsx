0f30efea0aa084a3057a6bb74bef8f7879204fd0
tyr0wl <paul.ch87@gmail.com>
2020-09-21 23:57:26 +0200

    Refactor Board into separate module

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { Square } from './Square';
+import { Board } from "./Board";
+import { SquareValue } from "./SquareValue";
 
-type SquareValue = (string | null);
 
 function calculateWinner(squares: SquareValue[]) {
     const lines = [
@@ -26,44 +26,6 @@ function calculateWinner(squares: SquareValue[]) {
     }
 
     return null;
-}
-
-type BoardProps = { squares: SquareValue[], onClick(index: number): void, line: number[] };
-
-class Board extends React.Component<BoardProps> {
-
-    constructor(props: BoardProps) {
-        super(props);
-        this.renderSquare = this.renderSquare.bind(this);
-    }
-
-    renderSquare(index: number) {
-        return <Square
-            key={"square" + index}
-            value={this.props.squares[index]}
-            winner={this.props.line.indexOf(index) !== -1}
-            onClick={() => this.props.onClick(index)}
-        />;
-    }
-
-    render() {
-        let cellNumber = 0;
-        const rows = [];
-        for (let index = 0; index < 3; index++) {
-            const cells = [];
-            for (let indexJ = 0; indexJ < 3; indexJ++) {
-                cells.push(this.renderSquare(cellNumber));
-                cellNumber++;
-            }
-
-            const row = <div key={"board-row" + index} className="board-row">{cells}</div>;
-            rows.push(row);
-        }
-
-        return (
-            <div>{rows}</div>
-        );
-    }
 }
 
 type HistoryEntry = { squares: SquareValue[], step: number, };
