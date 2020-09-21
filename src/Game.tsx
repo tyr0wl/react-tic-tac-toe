4f544527b08b1fff796b9ecc2c0b6251a0442c51
tyr0wl <paul.ch87@gmail.com>
2020-09-22 00:00:52 +0200

    Refactor calculateWinner into separate module

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,29 +1,7 @@
 import { SquareValue } from "./SquareValue";
 import React from "react";
 import { Board } from "./Board";
-
-export function calculateWinner(squares: SquareValue[]) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-
-    for (const line of lines) {
-        const [a, b, c] = line;
-
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return { player: squares[a], squares: line, };
-        }
-    }
-
-    return null;
-}
+import { calculateWinner } from "./calculateWinner";
 
 export type GameState = { history: HistoryEntry[], move: number, xIsNext: boolean, sortAsc: boolean, winner: { player: SquareValue, squares: number[] } | null };
 type HistoryEntry = { squares: SquareValue[], step: number, };
